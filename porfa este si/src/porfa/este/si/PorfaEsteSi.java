cff2b105bd6d0ba1f17d217b8a49b38cb7d86c30
LicQuiterio <114037872+Fridanahuat@users.noreply.github.com>
2022-09-26 04:48:17 -0500

    Inicio parte 2

---
+++ porfa este si/src/porfa/este/si/PorfaEsteSi.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package porfa.este.si;
+
+/**
+ *
+ * @author jesus
+ */
+public class PorfaEsteSi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ventana ver = new ventana();
+        ver.setVisible(true);
+        ver.setLocationRelativeTo(null);
+    }
+    
+}
