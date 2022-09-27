31e837c7d0371aa61fc4f9c6c57c79282d09df41
valentina978 <114037877+valentina978@users.noreply.github.com>
2022-09-27 11:50:25 -0500

    prueba 1

--- INICIO/src/inicio/INICIO.java
+++ INICIO/src/inicio/INICIO.java
@@ -15,7 +15,7 @@ public class INICIO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("kuydgh");
     }
     
 }
