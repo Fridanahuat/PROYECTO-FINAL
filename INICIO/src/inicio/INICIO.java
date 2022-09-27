56daf4695b2d08b779d5a53c23036c501ac79aeb
LicQuiterio <114039578+LicQuiterio@users.noreply.github.com>
2022-09-26 22:19:34 -0500

    Login completo
    Login ya con todas las funcionalidades
    
    Co-Authored-By: valentina978 <114037877+valentina978@users.noreply.github.com>

---
+++ INICIO/src/inicio/INICIO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inicio;
+
+/**
+ *
+ * @author jesus
+ */
+public class INICIO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         ventana ver = new ventana();
+        ver.setVisible(true);
+        ver.setLocationRelativeTo(null);
+    }
+    
+}
